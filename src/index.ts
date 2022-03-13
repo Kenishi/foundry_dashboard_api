466fae0ac72b2a376fadfd76963965dda2d54878
Jeremy May <kenishi86@gmail.com>
2022-03-13 16:52:38 -0400

    Fix import bug

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import express from "express";
-import * as Docker from "dockerode";
+import Docker from "dockerode";
 
 const docker = new Docker({socketPath: "/var/run/docker.sock"});
 const app = express();
